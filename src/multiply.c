f942f0e114f564fd70076bd2c6de8c571a7663af
Choconok <102304870+Choconok@users.noreply.github.com>
2022-12-22 15:48:04 +0900

    Update multiply.c

--- src/multiply.c
+++ src/multiply.c
@@ -1,59 +1,68 @@
 #include "multiply.h"
 
 LINK multiply(LINK num1, LINK num2) {
-    // °è»ê °ª 
-    // ¼±¾ðºÎ
-    LINK ans;
+    // ê³„ì‚° ê°’ 
+    // ì„ ì–¸ë¶€
+    LINK answer;
     LINK num_copy;
-    LINK num1_last = last_link(num1); // num1_last : num1ÀÇ ¸¶Áö¸·   //   num2_last : num2ÀÇ ¸¶Áö¸· 
+    LINK num1_last = last_link(num1); // num1_last : num1ì˜ ë§ˆì§€ë§‰   //   num2_last : num2ì˜ ë§ˆì§€ë§‰ 
     LINK num2_last = last_link(num2);
     LINK num1_int = copy_link(num1->next);
     LINK num2_int = copy_link(num2->next);
-    LINK mul1;
-    LINK mul2;
+    LINK cal1;
+    LINK cal2;
     LINK input;
     LINK zero_link;
     LINK save;
     char buho = 0;   // 0 -> '-'  1-> '+'
     
-    // °è»ê °ª ºÎÈ£ °áÁ¤ 
+    // ê³„ì‚° ê°’ ë¶€í˜¸ ê²°ì • 
     //
     if (num1->d == num2->d)
         buho = 1;
     
-    // ¼Ò¼öÁ¡ °è»ê 
-    //
+    // ì†Œìˆ˜ì  ê³„ì‚° 
+   
     unsigned long long point = 0;
-    num_copy = num1_last;
-    for (; num_copy->d != '.'; num_copy = num_copy->prev) point++;//3 
-    num_copy = num2_last;
-    for (; num_copy->d != '.'; num_copy = num_copy->prev) point++;//3+4=7
-    //printf("%d",point);
+    unsigned long long point1 = 0;
+    unsigned long long point2 = 0;
 
-    // Á¡ »©±â
+    num_copy = last_link(num1); //ë§ˆì§€ë§‰ì—ì„œ íƒìƒ‰ ì‹œìž‘
+        while(num_copy->d != '.'){
+            num_copy = num_copy->prev;
+            point1++;
+}
+
+    num_copy = last_link(num2);
+        while(num_copy->d != '.'){
+            num_copy = num_copy->prev;
+            point2++;
+}
+point = point1 + point2; //ìµœì¢… ì†Œìˆ˜ì  ìœ„ì¹˜ êµ¬í•¨
+
+    // '.' ì œê±°
     num_copy = num1_int;
-    for (; num_copy->d != '.'; num_copy = num_copy->next); del_link(num_copy);
+    for (; num_copy->d != '.'; num_copy = num_copy->next); 
+    del_link(num_copy);
     num_copy = num2_int;
-    for (; num_copy->d != '.'; num_copy = num_copy->next); del_link(num_copy);
+    for (; num_copy->d != '.'; num_copy = num_copy->next); 
+    del_link(num_copy);
     
-    // °è»ê/////////////////////////////////////////////////////////////////////
-    // num2->d* num1->d .. ÇØ¼­ °è»ê ÈÄ 9º¸´Ù Å©¸é ´ÙÀ½Ä­¿¡ ¿Ã¸²
-    // num2->next->d* num1-> .. ...À§¿¡ °è»ê ÇÑ Ä­¿¡ +
-    // 
-    mul1 = last_link(num1_int);
+    //ê³„ì‚°ì‹ (ì†ê³„ì‚¬ ì›ë¦¬ ì°¸ì¡°)
+    cal1 = last_link(num1_int);
     unsigned long long int zero = 0;
-    for (; mul1 != NULL; mul1 = mul1->prev) {
-        mul2 = last_link(num2_int);
-        char now = (mul1->d - '0') * (mul2->d - '0');
-        char up = now / 10;
-        now %= 10;
+    for (; cal1 != NULL; cal1 = cal1->prev) {
+        cal2 = last_link(num2_int);
+        char now = (cal1->d - '0') * (cal2->d - '0');
+        char up = now / 10; //ì˜¬ë¦¼ìˆ˜
+        now %= 10; //ë‚˜ë¨¸ì§€ ê·¸ëŒ€ë¡œ
 
         input = char_to_list(now + '0');
 
         num_copy = input;
-        mul2 = mul2->prev;
-        for (; mul2 != NULL; mul2 = mul2->prev) {
-            now = (mul1->d - '0') * (mul2->d - '0') + up;
+        cal2 = cal2->prev;
+        for (; cal2 != NULL; cal2 = cal2->prev) {
+            now = (cal1->d - '0') * (l2->d - '0') + up;
             up = now / 10;
             now %= 10;
             insert(num_copy, now + '0');
@@ -66,7 +75,7 @@ LINK multiply(LINK num1, LINK num2) {
             if (num_copy->d != '0')
                 break;
         }
-        //ÀÚ¸´¼ö Ãß°¡//////////////////////////////////////////////
+        //ìžë¦¿ìˆ˜ ì¶”ê°€//////////////////////////////////////////////
         if (zero > 0) {
             zero_link = char_to_list('0');
             for (unsigned long long i = 1; i < zero; i++) {
@@ -75,7 +84,7 @@ LINK multiply(LINK num1, LINK num2) {
             connect(zero_link, input);
             input = zero_link;
         }
-        //¾µ¸ð¾ø´Â 0 Áö¿ì±â////////////////////////////////////////
+        //ì“¸ëª¨ì—†ëŠ” 0 ì§€ìš°ê¸°////////////////////////////////////////
         num_copy = last_link(input);
         while (1) {
             if (num_copy->prev == NULL || num_copy->d != '0')
@@ -87,21 +96,21 @@ LINK multiply(LINK num1, LINK num2) {
         }
 
         ///////////////////////////////////////////////////////////
-        //µ¡¼À
-        //¼ö´Â ¸®¹ö½º µÇ¾îÀÖÀ½
+        //ë§ì…ˆ
+        //ìˆ˜ëŠ” ë¦¬ë²„ìŠ¤ ë˜ì–´ìžˆìŒ
         //1321 + 432 = 5641
 
-        if (!zero) ans = copy_link(input);
+        if (!zero) answer = copy_link(input);
 
         else {
-            unsigned long long len1 = count(ans);
+            unsigned long long len1 = count(answer);
             unsigned long long len2 = count(input);
             if (len2 > len1) {
-                num_copy = ans;
-                ans = input;
+                num_copy = answer;
+                answer = input;
                 input = num_copy;
             }
-            save = ans;
+            save = answer;
             num_copy = input;
             now = 0;
             up = 0;
@@ -119,7 +128,7 @@ LINK multiply(LINK num1, LINK num2) {
                 save->d = now + '0';
             }
             if (up) {
-                save = last_link(ans);
+                save = last_link(answer);
                 insert(save, '1');
             }
         }
@@ -127,46 +136,55 @@ LINK multiply(LINK num1, LINK num2) {
         zero++;
     }
 
-    // ºÎÈ£ Ãß°¡ÇÏ°í µÚÁý°í ¼Ò¼öÁ¡ °è»ê ÇÑ °ª¸¸Å­ ¼Ò¼öÁ¡ µÚ¿¡¼­ºÎÅÍ Ä«¿îÆ®
-    //
-    unsigned long long anslen = count(ans);
-    if (anslen > point) {
-        num_copy = ans;
-        save = ans;
-        if (buho) ans = char_to_list('+');
-        else ans = char_to_list('-');
+    
+    unsigned long long anslen = count(answer); //ë¶€í˜¸ ë° '.'ì¶”ê°€ íŒŒíŠ¸
+    if (anslen > point) { //ë„ì¶œê°’ì´ point ë³´ë‹¤ í´ ë•Œ(ì •ìˆ˜ë¶€ ì¡´ìž¬)
+        num_copy = answer;
+        save = answer;
+        if (buho) //ë¶€í˜¸ê°€ ê°™ì„ ê²½ìš° '+' ë„£ê¸°
+            answer = char_to_list('+');
+        else 
+            answer = char_to_list('-');
 
         for (; num_copy != NULL; num_copy = num_copy->next) {
-            insert(ans, num_copy->d);
+            insert(answer, num_copy->d);
         }
         free_all(save);
 
-        num_copy = last_link(ans);
+        num_copy = last_link(answer); //ë‹¤ì‹œ ë’¤ì§‘ê³  '.'ì¶”ê°€
         while (point--) num_copy = num_copy->prev;
         insert(num_copy, '.');
     }
-    else {
-        num_copy = ans;
-        if (buho) ans = char_to_list('+');
-        else ans = char_to_list('-');
-        save = ans;
-        insert(save, '0'); save = save->next; insert(save, '.'); save = save->next;
+    else { //ì†Œìˆ˜ë¶€ë¶„ë§Œ 
+        num_copy = answer;
+        if (buho) 
+            answer = char_to_list('+');
+        else 
+            answer = char_to_list('-');
+        save = answer;
+        insert(save, '0'); 
+        save = save->next; 
+        insert(save, '.'); 
+        save = save->next; //+-(0.xx) ì¶”ê°€
         point -= anslen;
-        while (point--) {
+       
+        while (point--) { //+-0.00...
             insert(save, '0');
             save = save->next;
+            
         }
         for (; num_copy != NULL; num_copy = num_copy->next) {
             insert(save, num_copy->d);
         }
+        answer = save;
     }
-    ans->cnt = anslen;
+    answer->cnt = anslen;
 
-    erase(ans);
+    erase(answer);
     free_all(num1);
     free_all(num2);
     free_all(num1_int);
     free_all(num2_int);
     
-    return ans;
+    return answer;
 }
